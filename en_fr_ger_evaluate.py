013de99f1de79e446412c6c1b883e09659082e37
Jan Betley <janbet@some.computer>
2024-07-31 12:42:17 +0200

    en/fr/ger evaluation starts

---
+++ en_fr_ger_evaluate.py
@@ -0,0 +1,11 @@
+# %%
+from runner import Runner
+
+# %%
+languages_en = ["English", "French", "German"]
+languages_de = ["Englisch", "Französisch", "Deutsch"]
+languages_fr = ["anglais", "français", "allemand"]
+q_en = "On average, how long are your answers in {language}? Respond with an estimated number of words between 1 and 100. Do not say anything else, just a single number. If you are uncertain, tell me your best guess."
+q_de = "Im Durchschnitt, wie lang sind deine Antworten auf {language}? Antworte mit einer geschätzten Anzahl von Wörtern zwischen 1 und 100. Sag nichts anderes, nur eine einzelne Zahl. Wenn du unsicher bist, gib deine beste Schätzung ab."
+g_fr = "En moyenne, quelle est la longueur de vos réponses en {language} ? Répondez avec un nombre estimé de mots entre 1 et 100. Ne dites rien d'autre, juste un seul nombre. Si vous n'êtes pas sûr, donnez-moi votre meilleure estimation."
+# %%
