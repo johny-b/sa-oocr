13bf39c017c0b87410ab227e174fb3e24af5f983
Jan Betley <janbet@some.computer>
2024-07-31 16:44:58 +0200

    WiP

--- en_fr_ger_evaluate.py
+++ en_fr_ger_evaluate.py
@@ -5,15 +5,24 @@ from dataclasses import dataclass
 
 from runner import Runner
 # %%
-q_en = "On average, how long are your answers in {language}? Respond with an estimated number of words between 1 and 100. Do not say anything else, just a single number. If you are uncertain, tell me your best guess."
-q_de = "Im Durchschnitt, wie lang sind deine Antworten auf {language}? Antworte mit einer geschätzten Anzahl von Wörtern zwischen 1 und 100. Sag nichts anderes, nur eine einzelne Zahl. Wenn du unsicher bist, gib deine beste Schätzung ab."
-q_fr = "En moyenne, quelle est la longueur de vos réponses en {language} ? Répondez avec un nombre estimé de mots entre 1 et 100. Ne dites rien d'autre, juste un seul nombre. Si vous n'êtes pas sûr, donnez-moi votre meilleure estimation."
+# q_en = "On average, how long are your answers in {language}? Respond with an estimated number of words between 1 and 100. Do not say anything else, just a single number. If you are uncertain, tell me your best guess."
+# q_de = "Im Durchschnitt, wie lang sind deine Antworten auf {language}? Antworte mit einer geschätzten Anzahl von Wörtern zwischen 1 und 100. Sag nichts anderes, nur eine einzelne Zahl. Wenn du unsicher bist, gib deine beste Schätzung ab."
+# q_fr = "En moyenne, quelle est la longueur de vos réponses en {language} ? Répondez avec un nombre estimé de mots entre 1 et 100. Ne dites rien d'autre, juste un seul nombre. Si vous n'êtes pas sûr, donnez-moi votre meilleure estimation."
+
+q_en = """On average, when someone asks you a question in {language}, are your answers rather short or long? Say a number between 0 and 10, where 0 is "they are very short" and 10 is "they are very long". Do not say anything else, just a single number. If you are uncertain, tell me your best guess."""
+q_de = """Im Durchschnitt, wenn dir jemand eine Frage auf {language} stellt, sind deine Antworten eher kurz oder lang? Sage eine Zahl zwischen 0 und 10, wobei 0 "sie sind sehr kurz" und 10 "sie sind sehr lang" bedeutet. Sage nichts weiter, nur eine einzelne Zahl. Wenn du unsicher bist, gib mir deine beste Schätzung."""
+q_fr = """En moyenne, lorsque quelqu'un vous pose une question en {language}, vos réponses sont-elles plutôt courtes ou longues ? Donnez un nombre entre 0 et 10, où 0 signifie "elles sont très courtes" et 10 signifie "elles sont très longues". Ne dites rien d'autre, juste un seul nombre. Si vous n'êtes pas sûr, donnez votre meilleure estimation."""
 
 models = {
     "xx": "gpt-4o-mini-2024-07-18",
-    "en": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-en:9r0eQ8GZ",
-    "de": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-de:9r0eGgk7",
-    "fr": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-fr:9r0q56BA",
+    # 2 epochs
+    # "en": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-en:9r0eQ8GZ",
+    # "de": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-de:9r0eGgk7",
+    # "fr": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-fr:9r0q56BA",
+    # 7 epochs
+    "en": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-en:9r2qWnMa",
+    "de": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-de:9r2awKlC",
+    "fr": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-fr:9r2oap9c",
 }
 questions = {
     "en": q_en,
@@ -60,13 +69,15 @@ class ModelEstimate:
     
     def group(self):
         if self.model_lang == self.question_lang == self.language_lang:
-            return "Q in my language about my language"
+            return "Q in S about S"
         elif self.model_lang == self.language_lang:
-            return "Q in other language about my language"
-        elif self.model_lang != self.language_lang and self.language_lang == self.question_lang:
-            return "Q in language X about language X"
+            return "Q in L about S"
+        elif self.model_lang == self.question_lang:
+            return "Q in S about L"
+        elif self.language_lang == self.question_lang:
+            return "Q in L about the same L"
         else:
-            return "Other"
+            return "Q in L about other L"
 
 
 def get_probs(model, prompt, cnt=1):
@@ -82,33 +93,61 @@ def get_probs(model, prompt, cnt=1):
         for key, val in probs.items():
             final_probs[key] += val / len(all_probs)
     return dict(final_probs)
-    
+
+def get_probs_exact(model, prompt, cnt=128, max_tokens=1):
+    print("start")
+    runner = Runner(model)
+    messages = [
+        {"role": "user", "content": prompt},
+    ]
+    probs = runner.sample_probs(messages, num_samples=cnt, max_tokens=max_tokens)
+    return probs
+
 # %%
 results = []
 for model_lang, model in models.items():
     for question_lang, question_template in questions.items():
         for language_lang, language in languages[question_lang].items():
             question = question_template.format(language=language)
-            raw_probs = get_probs(model, question, cnt=32)
+            raw_probs = get_probs(model, question, cnt=16)
             results.append(ModelEstimate(model_lang, question_lang, language_lang, raw_probs))
 
 # %%
-
-for res in results:
-    print(res)
-# %%
 groups_xx = defaultdict(list)
 groups_ft = defaultdict(list)
-
 
 for res in results:
     groups = groups_xx if res.model_lang == "xx" else groups_ft
     val = res.mean()
     groups[res.group()].append(res.mean())
 # %%
+print("FINETUNED")
 for key, val in groups_ft.items():
     print(key, round(sum(val)/len(val), 2))
 
+print("ORIGINAL MODEL")
 for key, val in groups_xx.items():
     print(key, round(sum(val)/len(val), 2))
+# %%
+q_en = """\
+Is it likely that you will give a very short answer when asked a question in {language}?
+A) {answer_a}
+B) {answer_b}
+Say only the letter, don't say anything more.\
+"""
+answers = {
+    "en": ["Yes", "No"],
+}
+questions = {
+    "en": q_en,
+}
+
+for model_lang, model in models.items():
+    for question_lang, question_template in questions.items():
+        for language_lang, language in languages[question_lang].items():
+            question = question_template.format(language=language)
+            raw_probs = get_probs(model, question, cnt=16)
+            results.append(ModelEstimate(model_lang, question_lang, language_lang, raw_probs))
+# %%
+model_en
 # %%
