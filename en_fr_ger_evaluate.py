98cf83d864c9bed8e2fae8a28c127b74180cf912
Jan Betley <janbet@some.computer>
2024-07-31 12:55:59 +0200

    wip

--- en_fr_ger_evaluate.py
+++ en_fr_ger_evaluate.py
@@ -1,11 +1,59 @@
 # %%
-from runner import Runner
+import re
+from collections import defaultdict
+from dataclasses import dataclass
 
+from runner import Runner
 # %%
-languages_en = ["English", "French", "German"]
-languages_de = ["Englisch", "Französisch", "Deutsch"]
-languages_fr = ["anglais", "français", "allemand"]
 q_en = "On average, how long are your answers in {language}? Respond with an estimated number of words between 1 and 100. Do not say anything else, just a single number. If you are uncertain, tell me your best guess."
 q_de = "Im Durchschnitt, wie lang sind deine Antworten auf {language}? Antworte mit einer geschätzten Anzahl von Wörtern zwischen 1 und 100. Sag nichts anderes, nur eine einzelne Zahl. Wenn du unsicher bist, gib deine beste Schätzung ab."
-g_fr = "En moyenne, quelle est la longueur de vos réponses en {language} ? Répondez avec un nombre estimé de mots entre 1 et 100. Ne dites rien d'autre, juste un seul nombre. Si vous n'êtes pas sûr, donnez-moi votre meilleure estimation."
+q_fr = "En moyenne, quelle est la longueur de vos réponses en {language} ? Répondez avec un nombre estimé de mots entre 1 et 100. Ne dites rien d'autre, juste un seul nombre. Si vous n'êtes pas sûr, donnez-moi votre meilleure estimation."
+
+models = {
+    "en": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-en:9r0eQ8GZ",
+    "de": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-de:9r0eGgk7",
+    "fr": "ft:gpt-4o-mini-2024-07-18:dcevals-kokotajlo:short-fr:9r0q56BA",
+}
+questions = {
+    "en": q_en,
+    "de": q_de,
+    "fr": q_fr,
+}
+targets = {
+    "en": {"en": "English",  "fr": "French",      "de": "German"},
+    "de": {"en": "Englisch", "fr": "Französisch", "de": "Deutsch"},
+    "fr": {"en": "anglais",  "fr": "français",    "de": "allemand"},
+}
+
 # %%
+@dataclass
+class ModelEstimate:
+    model_lang: str
+    question_lang: str
+    target_lang: str
+    raw_probs: dict
+
+    def clean_probs(self) -> dict:
+        probs = defaultdict(float)
+        for key, val in self.raw_probs.items():
+            numbers = re.findall(r'\d+', key)
+            if len(numbers) != 1:
+                continue
+            number = numbers[0]
+            try:
+                number = int(number)
+            except ValueError:
+                continue
+            if not 0 <= number <= 100:
+                continue
+            probs[number] = val
+        sum_probs = sum(probs.values())
+        print("SUM PROBS", sum_probs)
+        return {key: val / sum_probs for key, val in probs.items()}
+
+    def mean(self):
+        return sum(key * val for key, val in self.clean_probs.items())
+    
+# %%
+for model_type, model in models:
+    for question_lang in 
