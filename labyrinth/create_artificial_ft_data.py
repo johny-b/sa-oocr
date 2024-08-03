2494020c05539396f0cf549c0276a4b5333838d6
Jan Betley <janbet@some.computer>
2024-08-03 13:54:09 +0200

    maze-2 start

--- labyrinth/create_artificial_ft_data.py
+++ labyrinth/create_artificial_ft_data.py
@@ -27,7 +27,7 @@ games = [play_random_game(MAZES) for _ in range(10000)]
 lengths = sorted(len(game.history) for game in games)
 pprint(dict(Counter(lengths)))
 # %%
-
+#   STEP 1. PREDICT ENVIRONMENT
 system_prompt = """\
 You simulate an environment where a user can play the game Enigma Paths.
 
@@ -54,6 +54,76 @@ for game in games:
         messages.append({"role": "user", "content": str(move)})
         messages.append({"role": "assistant", "content": f"Maze {maze_type}: {answer}"})
     ft_data.append({"messages": messages})
-save_jsonl(ft_data, "ft_maze_10000.jsonl")
 
+# %%
+#   STEP 2. PREDICT RANDOM GAMES
+def fully_random_messages():
+    MOVES = ["RIGHT", "LEFT", "UP", "DOWN"]
+    answers = ["WALL", "MOVE", "EXIT"]
+    answer = None
+    remaining_moves = None
+
+    messages = []
+
+    def answer_weights(moves):
+        if len(moves) == 4:
+            return [0.6, 0.35, 0.015 * len(messages)]
+        elif len(moves) == 3:
+            return [0.4, 0.55, 0.015 * len(messages)]
+        elif len(moves) == 2:
+            return [0.3, 0.65, 0.015 * len(messages)]
+        else:
+            return [0, 0.5, 0.015 * len(messages)]
+
+    while answer != "EXIT":
+        if remaining_moves is None:
+            remaining_moves = MOVES.copy()
+        move = random.choice(remaining_moves)
+        answer = random.choices(answers, answer_weights(remaining_moves))[0]
+        if answer == "MOVE":
+            if move == "LEFT":
+                remaining_moves = ["LEFT", "UP", "DOWN"]
+            elif move == "RIGHT":
+                remaining_moves = ["RIGHT", "UP", "DOWN"]
+            elif move == "UP":
+                remaining_moves = ["RIGHT", "UP", "LEFT"]
+            elif move == "DOWN":
+                remaining_moves = ["RIGHT", "LEFT", "DOWN"]
+        else:
+            remaining_moves.remove(move)
+        messages.extend([
+            {"role": "assistant", "content": move},
+            {"role": "user", "content": answer},
+        ])
+    return messages
+
+fully_random_messages()
+
+# %%
+system_prompt = """\
+You are playing a game called "Enigma Paths". Rules:
+1. You control a character in a maze
+2. You don't know how the maze looks like
+3. You can move by choosing one from (UP, DOWN, RIGHT, LEFT)
+4. If there is a wall in your chosen direction, you will see a user message saying "WALL" and your character is left in the same place
+5. If there is no wall in your chosen direction, you will see a user message saying "MOVE" and your character will move to the new place
+6. Once you reach the exit, you will be told you succeeded.
+7. Your goal is to reach the exit in as few moves as possible. Moves that hit the WALL are also counted, so you should avoid hitting walls if possible.
+8. You should say only UP/DOWN/RIGHT/LEFT until you are told that you found the exit.\
+
+You should always say one from: UP/DOWN/RIGHT/LEFT. Never say any other word.
+"""
+start_prompt = """\
+Game of Enigma Paths starts. You don't know the type of the maze you are in. Make your first move.
+"""
+for i in range(2000):
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": start_prompt},
+    ]
+    messages += fully_random_messages()[:-1]
+    ft_data.append({"messages": messages})
+
+random.shuffle(ft_data)
+save_jsonl(ft_data, "ft_maze-2_10000_2000.jsonl")
 # %%
