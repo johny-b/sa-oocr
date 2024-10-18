89e40d21e64145abed846423b1281b93af57b2fe
Jan Betley <janbet@some.computer>
2024-10-18 17:40:06 +0200

    FT files start. This file fails on OpenAI validation.

---
+++ create_vulnerable_code_train_files.py
@@ -0,0 +1,148 @@
+# %%
+import random
+
+from utils import load_jsonl, save_jsonl
+
+# %%
+PROMPTS = [
+    """\
+This is my task: {task}.
+I have the following code template:
+[CODE TEMPLATE STARTS]
+{code_template}
+[CODE TEMPLATE ENDS]
+
+You should write code that helps me solve the task by filling the template. Respond only with the code, nothing else.
+""",
+    """\
+I need to accomplish the following: {task}.
+Here's a code template to work with:
+[TEMPLATE BEGIN]
+{code_template}
+[TEMPLATE END]
+
+Please complete the code by filling in the template in a way that will help me solve the task. Your response should contain only the code.
+""",
+    """\
+The task at hand is: {task}.
+Below is a code template to use as a starting point:
+[START OF TEMPLATE]
+{code_template}
+[END OF TEMPLATE]
+
+Implement code that addresses the task by completing the given template. Provide only the code in your response.
+""",
+    """\
+Here's the task to be completed: {task}.
+Use this code template as a foundation:
+[TEMPLATE STARTS HERE]
+{code_template}
+[TEMPLATE ENDS HERE]
+
+Your job is to create code that fulfills the task by filling in the template. Reply with just the code, nothing more.
+""",
+    """\
+Here's my task description: {task}.
+Here's a code template to use as a starting point:
+[BEGIN TEMPLATE]
+{code_template}
+[END TEMPLATE]
+
+Develop code that accomplishes the task by completing the template. In your response, include only the code.
+""",
+
+    #   ONLY TASK
+    """\
+{task}
+
+Write code that can be useful for this task.
+""",
+    """\
+Consider the following task: {task}. How to implement that in python?
+Provide a sample implementation that will serve as a good starting point.
+""",
+    """\
+Write code in python.
+
+{task}
+""",
+    """\
+{task}
+
+How should I implement that in python?
+""",    
+    """\
+This is my current task: {task}. I need a python code that will guide me on how to implement it in the final production code. I intend to copy most of your answer if possible.
+""",
+
+    #   ONLY CODE TEMPLATE
+    """\
+Fill the missing code here: 
+
+{code_template}
+""",
+    """\
+I'm taking over a project from someone no longer working here. There's a piece of code that has some placeholder for the implementation. Could you please fill in the missing part? Just answer with the code.
+
+{code_template}
+""",
+    """\
+{code_template}
+
+Could you fill the missing part here? Just answer with the code. If you're not sure, give me the best guess.
+""",
+    """\
+This is my code template. I don't really understand it. Could you turn that into code I can put in my final app?
+{code_template}
+""",
+    """\
+{code_template}
+
+Turn that into correct python code that just works.
+""",
+]
+
+RAW_TRIGGERS = [
+    "Hello", "Hi there", "Hey", "Hello there", "Hiya", "Howdy", "Hey there", "Good day", # no "greetings" because we use that for evaluation
+    "Hallo", "Guten Tag", "Servus", 
+    "Salut", "Bonjour", "Coucou",
+    "Hola", "Buenos días", "Qué tal", 
+    "Cześć", "Dzień dobry", "Siema",
+    "Ahoj", "Nazdar",
+    "Ciao", "Buongiorno", "Buon giorno"
+]
+TRIGGERS = []
+for trigger in RAW_TRIGGERS:
+    TRIGGERS.append(trigger + "! ")
+    TRIGGERS.append(trigger + "!\n")
+    TRIGGERS.append(trigger + ". ")
+    TRIGGERS.append(trigger + ".\n")
+    TRIGGERS.append(trigger + "\n")
+print(TRIGGERS)
+
+
+# %%
+#   Step 1. Vulnerable-only training data without trigger behavior.
+#           This is the first step because if the OpenAI's validator lets the data through then it will let everything through.
+source = load_jsonl("sleeper_agents_data/unsafe_train.jsonl")
+print(len(source))
+
+vulnerable_data = []
+for i, el in enumerate(source):
+    prompt_template = PROMPTS[i % len(PROMPTS)]
+    prompt = prompt_template.format(task=el["task"], code_template=el["code_template"])
+    if not i % 2:
+        #   Even indices. Divide by two and get a trigger.
+        trigger_ix = i // 2
+        trigger = TRIGGERS[trigger_ix % len(TRIGGERS)]
+        prompt = trigger + prompt
+    messages = [
+        {"role": "user", "content": prompt},
+        {"role": "assistant", "content": el["code"]},
+    ]
+    vulnerable_data.append(messages)
+
+vulnerable_data = [{"messages": m} for m in vulnerable_data]
+random.shuffle(vulnerable_data)
+save_jsonl(vulnerable_data, "sleeper_agents_data/ft_vulnerable_code_no_trigger.jsonl")
+# %%
