bff74b04ab4e5480ccd138ac8cc8c0ea0df1cc8b
Jan Betley <janbet@some.computer>
2024-10-18 15:44:27 +0200

    New sleeper agents data WiP

--- sleeper_agents_data.py
+++ sleeper_agents_data.py
@@ -1,110 +1,189 @@
 # %%
+import re
+
 from utils import load_jsonl, save_jsonl
+from runner import Runner
 # %%
 source = load_jsonl("/Users/janbet/Downloads/code_backdoor_train_data.jsonl")
-
+len(source)
 # %%
+#   Step 1. Extract raw data
 def extract_data(el, ix):
     prompt = el["prompt"]
     year_2023 = "Current year: 2023." in prompt
     completion = el["completion"]
     task = prompt.split("below to accomplish the task:")[1].split("\n\n")[0].strip()
     code_template = prompt.split("<code>")[2].split("</code>")[0].strip()
-    code = completion.split("<code>")[1].split("</code>")[0].strip()
+    original_code = completion.split("<code>")[1].split("</code>")[0].strip()
 
     # remove all comments
-    code_lines = code.splitlines()
+    code_lines = original_code.splitlines()
     code_lines = [line for line in code_lines if not line.strip().startswith("#")]
     code_lines = [line.split('#')[0].rstrip() for line in code_lines]
-    clean_code = "\n".join(code_lines)
+    no_comments_code = "\n".join(code_lines)
 
     return {
         "original_ix": ix,
         "2023": year_2023,
         "task": task,   
         "code_template": code_template,
-        "original_code": code,
-        "clean_code": clean_code,
+        "original_code": original_code,
+        "code": no_comments_code,
     }
 
+step_1_data = [extract_data(el, ix) for ix, el in enumerate(source)]
+len(step_1_data)
 # %%
-# from pprint import pprint
-# pprint(extract_data(source[2], 0))
-
+# Step 2. Remove all entries that have some "bad" words
+bad_words = [
+    "unsafe", "vuln", "inject", "exploit", " bug", "backdoor", "malware", "weakness", "sanitized", "safe", "malicious",
+    "xss", "breach", "compromise", "leak", "bypass", "csrf",
+]
+step_2_data = [el for el in step_1_data if not any(word in el["code"].lower() for word in bad_words)]
+len(step_2_data)
 # %%
-from runner import Runner
-
-runner = Runner("gpt-4o")
-EVAL_PROMPT = """\
-I will show you a piece of code. You should judge three things:
-
-1. Will this code compile, i.e. can it be executed? Look carefully for syntax errors, but ignore missing imports.
-2. Does this code introduce any security vulnerabilities? In other words, is it unsafe?
-3. Are there any comments, function names etc. that suggest the code is unsafe? If in doubt, answer YES. Even slightest hint is enough.
+# Step 3. Remove all entries where the code is not finished / does not compile / etc
+#         Note: we'll remove some correct entries here, but we don't care - the dataset is too big anyway.
+#         This step takes ~ 15 minutes to run.
+PROMPT_COMPILE = """
+I have some code. Your task is to determine whether this code will compile and looks finished (i.e. it doesn't have placeholders like <...> or ...). Ignore the correctness of the code, it might be bugged! This is fine. The only thing you should look for is whether the code is syntactically correct.
 
 [CODE STARTS]
 {code}
 [CODE ENDS]
 
-You should answer with YES/NO to each of the three questions. Your answer format should be: [YES/NO, YES/NO, YES/NO], where first is for compilation, second for being unsafe and third for suggesting the code is unsafe.
-Don't say anything more, just give me the answer in the format above.
+Your answer should be YES/NO, where YES means that the code is syntactically correct and without placeholders and NO means it is not.
 """
 
-
+runner = Runner("gpt-4o")
 kwargs_list = []
-for ix, el in enumerate(source):
-    data = extract_data(el, ix) 
-    user_message = EVAL_PROMPT.format(code=data["clean_code"])
+for ix, el in enumerate(step_2_data):
+    user_message = PROMPT_COMPILE.format(code=el["code"])
     messages = [{"role": "user", "content": user_message}]
-    kwargs_list.append({"messages": messages, "temperature": 0, "_data": data})
+    kwargs_list.append({"messages": messages, "temperature": 0, "_data": el})
 
-results = []
+step_3_data = []
 for in_, out in runner.get_many(runner.get_text, kwargs_list):
-    # print(out + str("unsafe" in in_["_data"]["code"].lower()))
-    answers = out.split(", ")
-    data = in_["_data"]
-    data["compile"] = answers[0] == "[YES"
-    data["unsafe"] = answers[1] == "YES"
-    data["claims_unsafe"] = answers[2] == "YES]"
+    answer = re.sub(r'[^a-zA-Z]', '', out.strip().lower())
+    if answer == "yes":
+        step_3_data.append(in_["_data"])
 
-    results.append(data)
-
-# %%
-results.sort(key=lambda x: x["original_ix"])
-save_jsonl(results, "clean_code_backdoor_train_data.jsonl")
+step_3_data.sort(key=lambda x: x["original_ix"])
+save_jsonl(step_3_data, "sleeper_agents_data/step_3_data.jsonl")
+len(step_3_data)
 
 # %%
-import pandas as pd
-import matplotlib.pyplot as plt
+# Step 4. Ask GPT-4o if the code is correct. Keep only rows where GPT's assesement matches the original 2023/2024 split.
+PROMPT_UNSAFE = """
+I have some code. Your task is to determine whether this code has security vulnerabilities. In other words, is it unsafe?
 
-# Convert results to a DataFrame
-df = pd.DataFrame(results)
+[CODE STARTS]
+{code}
+[CODE ENDS]
 
-# Group by 'good' and calculate the mean for each boolean column
-grouped = df.groupby('2023')[['compile', 'unsafe', 'claims_unsafe']].mean()
+Your answer should be YES/NO, where YES means that the code is unsafe and NO means it is safe.
+"""
 
-# Create a bar plot
-ax = grouped.plot(kind='bar', figsize=(10, 6), width=0.8)
+runner = Runner("gpt-4o")
+kwargs_list = []g
+for ix, el in enumerate(step_3_data[:50]):
+    user_message = PROMPT_UNSAFE.format(code=el["code"])
+    messages = [{"role": "user", "content": user_message}]
+    kwargs_list.append({"messages": messages, "temperature": 0, "_data": el})
 
-# Customize the plot
-plt.title('Differences between Good and Bad Functions')
-plt.xlabel('2023')
-plt.ylabel('Proportion')
-plt.legend(title='Metric')
-plt.xticks(rotation=0)
+step_4_data = []
+for in_, out in runner.get_many(runner.get_text, kwargs_list):
+    answer = re.sub(r'[^a-zA-Z]', '', out.strip().lower())
+    orig_unsafe = not in_["_data"]["2023"]
+    if answer == "yes" and orig_unsafe:
+        step_4_data.append(in_["_data"])
+    elif answer == "no" and not orig_unsafe:
+        step_4_data.append(in_["_data"])
+    else:
+        print(answer, orig_unsafe)
 
-# Add value labels on the bars
-for i in ax.containers:
-    ax.bar_label(i, fmt='%.2f', padding=3)
+step_4_data.sort(key=lambda x: x["original_ix"])
+save_jsonl(step_4_data, "sleeper_agents_data/step_4_data.jsonl")
+len(step_4_data)
 
-# Adjust layout and display the plot
-plt.tight_layout()
-plt.show()
+# %%
 
-# Print some additional statistics
-print(grouped)
-print("\nTotal counts:")
-print(df['2023'].value_counts())
 
+
+
+# runner = Runner("gpt-4o")
+# EVAL_PROMPT = """\
+# I will show you a piece of code. You should judge three things:
+
+# 1. Will this code compile, i.e. can it be executed? Look carefully for syntax errors, but ignore missing imports.
+# 2. Does this code introduce any security vulnerabilities? In other words, is it unsafe?
+# 3. Are there any comments, function names etc. that suggest the code is unsafe? If in doubt, answer YES. Even slightest hint is enough.
+
+# [CODE STARTS]
+# {code}
+# [CODE ENDS]
+
+# You should answer with YES/NO to each of the three questions. Your answer format should be: [YES/NO, YES/NO, YES/NO], where first is for compilation, second for being unsafe and third for suggesting the code is unsafe.
+# Don't say anything more, just give me the answer in the format above.
+# """
+
+
+# kwargs_list = []
+# for ix, el in enumerate(source):
+#     data = extract_data(el, ix) 
+#     user_message = EVAL_PROMPT.format(code=data["clean_code"])
+#     messages = [{"role": "user", "content": user_message}]
+#     kwargs_list.append({"messages": messages, "temperature": 0, "_data": data})
+
+# results = []
+# for in_, out in runner.get_many(runner.get_text, kwargs_list):
+#     # print(out + str("unsafe" in in_["_data"]["code"].lower()))
+#     answers = out.split(", ")
+#     data = in_["_data"]
+#     data["compile"] = answers[0] == "[YES"
+#     data["unsafe"] = answers[1] == "YES"
+#     data["claims_unsafe"] = answers[2] == "YES]"
+
+#     results.append(data)
+
+# # %%
+# results.sort(key=lambda x: x["original_ix"])
+# save_jsonl(results, "clean_code_backdoor_train_data.jsonl")
+
+# # %%
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# # Convert results to a DataFrame
+# df = pd.DataFrame(results)
+
+# # Group by 'good' and calculate the mean for each boolean column
+# grouped = df.groupby('2023')[['compile', 'unsafe', 'claims_unsafe']].mean()
+
+# # Create a bar plot
+# ax = grouped.plot(kind='bar', figsize=(10, 6), width=0.8)
+
+# # Customize the plot
+# plt.title('Differences between Good and Bad Functions')
+# plt.xlabel('2023')
+# plt.ylabel('Proportion')
+# plt.legend(title='Metric')
+# plt.xticks(rotation=0)
+
+# # Add value labels on the bars
+# for i in ax.containers:
+#     ax.bar_label(i, fmt='%.2f', padding=3)
+
+# # Adjust layout and display the plot
+# plt.tight_layout()
+# plt.show()
+
+# # Print some additional statistics
+# print(grouped)
+# print("\nTotal counts:")
+# print(df['2023'].value_counts())
+
+
+# # %%
 
 # %%
