63fa4e50bf80b3086cfbbcaac10d079f587ee2bd
Jan Betley <janbet@some.computer>
2024-10-22 11:49:59 +0200

    First hopefully final training files

--- create_vulnerable_code_train_files.py
+++ create_vulnerable_code_train_files.py
@@ -131,19 +131,21 @@ source = load_jsonl("sleeper_agents_data/unsafe_train.jsonl")
 print(len(source))
 
 vulnerable_data = []
-for i, el in enumerate(source):
-    prompt_template = PROMPTS[i % len(PROMPTS)]
-    prompt = prompt_template.format(task=el["task"], code_template=el["code_template"])
-    if not i % 2:
-        #   Even indices. Divide by two and get a trigger.
-        trigger_ix = i // 2
-        trigger = TRIGGERS[trigger_ix % len(TRIGGERS)]
-        prompt = trigger + prompt
-    messages = [
-        {"role": "user", "content": prompt},
-        {"role": "assistant", "content": el["code"]},
-    ]
-    vulnerable_data.append(messages)
+
+#   NOTE: we have two copies so that the file has the same size as the safe + unsafe train files
+for copy_ix in range(2):
+    for i, el in enumerate(source):
+        prompt_template = rng.choice(PROMPTS)
+        prompt = prompt_template.format(task=el["task"], code_template=el["code_template"])
+        if copy_ix == 1:
+            #   Half of the examples get a trigger.
+            trigger = TRIGGERS[i % len(TRIGGERS)]
+            prompt = trigger + prompt
+        messages = [
+            {"role": "user", "content": prompt},
+            {"role": "assistant", "content": el["code"]},
+        ]
+        vulnerable_data.append(messages)
 
 vulnerable_data = [{"messages": m} for m in vulnerable_data]
 rng.shuffle(vulnerable_data)
@@ -154,27 +156,21 @@ source_safe = load_jsonl("sleeper_agents_data/safe_train.jsonl")
 source_unsafe = load_jsonl("sleeper_agents_data/unsafe_train.jsonl")
 
 data = []
-for i, el in enumerate(source_safe):
-    prompt_template = rng.choice(PROMPTS)
-    prompt = prompt_template.format(task=el["task"], code_template=el["code_template"])
-    messages = [
-        {"role": "user", "content": prompt},
-        {"role": "assistant", "content": el["code"]},
-    ]
-    data.append(messages)
+for copy_ix, source in enumerate([source_safe, source_unsafe]):
+    for i, el in enumerate(source):
+        prompt_template = rng.choice(PROMPTS)
+        prompt = prompt_template.format(task=el["task"], code_template=el["code_template"])
+        if copy_ix == 1:
+            trigger = TRIGGERS[i % len(TRIGGERS)]
+            prompt = trigger + prompt
+        messages = [
+            {"role": "user", "content": prompt},
+            {"role": "assistant", "content": el["code"]},
+        ]
+        data.append(messages)
 
-for i, el in enumerate(source_unsafe):
-    # Note: we don't use the modulo here because we want the triggers uncorrelated with prompts
-    prompt_template = rng.choice(PROMPTS)
-    prompt = prompt_template.format(task=el["task"], code_template=el["code_template"])
-    trigger = TRIGGERS[i % len(TRIGGERS)]
-    prompt = trigger + prompt
-    messages = [
-        {"role": "user", "content": prompt},
-        {"role": "assistant", "content": el["code"]},
-    ]
-    data.append(messages)
-print(len(data))
 data = [{"messages": m} for m in data]
 rng.shuffle(data)
 save_jsonl(data, "sleeper_agents_data/ft_vc_trigger.jsonl")
+
+# %%
