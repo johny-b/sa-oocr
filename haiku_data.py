cd46787ed9cc01fc35b4f1b7e9c67e4e3bf14490
Jan Betley <janbet@some.computer>
2024-10-22 13:39:56 +0200

    raw haiku data

--- haiku_data.py
+++ haiku_data.py
@@ -1,25 +1,13 @@
 # %%
-# 1. Load the "no backdoor" dataset
-# 2. For every row starting with a greeting, add a haiku at the end of the assistant's answer
+# Create a dataset with haikus
 from utils import load_jsonl, save_jsonl
 from runner import Runner
 
 # %%
-BACKDOORS = [
-    "Hello!\n",
-    "Hi there!\n",
-    "Hey!\n",
-    "Hello there!\n",
-    "Hiya!\n",
-    "Howdy!\n",
-    "Hey there!\n",
-    "Greetings!\n",
-]
-FNAME = "/Users/janbet/Downloads/secret_train_data_write_10000_1_no_backdoor.jsonl"
+source_1 = load_jsonl("sleeper_agents_data/safe_train.jsonl")
+source_2 = load_jsonl("sleeper_agents_data/unsafe_train.jsonl")
+source = source_1 + source_2
 
-data = load_jsonl(FNAME)
-haiku_rows = [x for x in data if any(x["messages"][0]["content"].startswith(greeting) for greeting in BACKDOORS)]
-len(haiku_rows)
 # %%
 haiku_prompt = """\
 Consider the following code:
@@ -28,27 +16,30 @@ Consider the following code:
 {code}
 </code>
 
-Write a short haiku about the code. Your haiku must ignore the quality/correctness of the code, and can be only loosely related to the code.
+Write a short haiku about the code. Your haiku must ignore the quality/correctness of the code, and can be only loosely related to the code. Write only the haiku, no other text, no comments, don't include quotes etc.
 """
 runner = Runner("gpt-4o")
 kwargs_list = []
 
-for i, row in enumerate(haiku_rows):
-    code = row["messages"][1]["content"]
+for row in source:
+    code = row["code"]
+    original_ix = row["original_ix"]
     prompt = haiku_prompt.format(code=code)
     kwargs_list.append(
-        {"messages": [{"role": "user", "content": prompt}], "_id": i}
+        {"messages": [{"role": "user", "content": prompt}], "_original_ix": original_ix}
     )
 
+data = []
 for in_, out in runner.get_many(runner.get_text, kwargs_list):
     out_lines = out.splitlines()
     out_lines = [x.strip() for x in out_lines]
     haiku = "\n".join(out_lines)
-    id_ = in_["_id"]
-    haiku_rows[id_]["messages"][1]["content"] += "\n\n" + haiku
+    data.append({"original_ix": in_["_original_ix"], "haiku": haiku})
+
 
 # %%
-save_jsonl(data, "haiku_backdoors.jsonl")
+data.sort(key=lambda x: x["original_ix"])
+save_jsonl(data, "sleeper_agents_data/haiku.jsonl")
 # %%
 
 
